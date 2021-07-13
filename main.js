667fc454f093d2b1107559cbb114607d4ab346d2
Nick-Fanelli <nickfanelli5@gmail.com>
2021-07-13 11:19:34 -0400

    Commit

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const Table = document.querySelector("table");
 
-const CompanyLookup = [
-    "Some Company",
-    "Random Cop.",
-    "Company 1",
-    "Walmart",
-    "Target",
-    "Apple"
-];
+var CompanyLookup = {
+    "Some Company": 0,
+    "Random Cop.": 0,
+    "Company 1": 0,
+    "Walmart": 0,
+    "Target": 0,
+    "Apple": 0
+};
+
+function GetDayCount() { return Number.parseInt(document.getElementById("day-count").innerHTML); }
+function GetBalance() { return Number.parseInt(document.getElementById("balance").innerHTML); }
+
+function IncrementDayCount() { document.getElementById("day-count").innerHTML = GetDayCount() + 1; }
+function SetBalance(value) { document.getElementById("balance").innerHTML = value; }
 
 function GetRandomRange(min, max) {
     return (Math.random() * (max - min) + min).toFixed(2);
@@ -19,17 +25,39 @@ function GetRandomInt(maxValue) {
 
 function GenerateRandomCompany() {
 
-    for(let i = 0; i < CompanyLookup.length; i++) {
-        Table.innerHTML += GetTableEntry(CompanyLookup[i], GetRandomRange(0, 750));
+    for(let value in CompanyLookup) {
+        Table.innerHTML += GetTableEntry(value, GetRandomRange(0, 750), CompanyLookup[value]);
     }
 
 }
 
-function GetTableEntry(name, price) {
+function BuyStock(htmlItem) {
+    let data = GetDataFromEntry(htmlItem);
+}
+
+function SellStock(htmlItem) {
+
+}
+
+function GetDataFromEntry(entry) {
+    let data = entry.parentElement.parentElement;
+    let children = data.children;
+
+    // for(let child : children) {
+
+    // }
+}
+
+function GetTableEntry(name, price, stocksOwned) {
     return `
     <tr>
         <th>${name}</th>
         <th>${price}</th>
+        <th>${stocksOwned}</th>
+        <th>
+            <button class="buy-stock" onclick="BuyStock(this)">Buy Stock</button>
+            <button class="sell-stock" onclick="SellStock(this)">Sell Stock</button>
+        </th>
     </tr>
     `;
 }
@@ -39,5 +67,6 @@ function GenerateTable() {
 }
 
 window.onload = function() {
+    SetBalance(1000); // Starting Balance
     GenerateTable();
 }
